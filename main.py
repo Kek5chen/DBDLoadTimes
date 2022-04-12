cf41511115e59f283b67cdfb9b8749c9aa4a84f0
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-04-13 00:37:32 +0200

    added data plotting when exiting the program
    refactored
    
    updated data

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import atexit
 import numpy as np
+import pandas as pd
 import pyscreenshot as ps
 import pygetwindow as gw
 import time
 import json
+import plotly.express as px
 
 
 def takeDbdScreenshot():
@@ -26,55 +29,80 @@ def takeDbdScreenshot():
 def detectMap(mapLoadImg):
     if mapLoadImg.getpixel(156, 955) == (108, 111, 114):
         return "ormond"
+    else:
+        return ""
 
 
 loadingTimes = json.load(open("loadingTimes.json"))
-
 possibleMaps = json.load(open("possibleMaps.json"))
 
+
+def drawLoadingTimesBarChart():
+    average_loading_times = list()
+    map_translations = list()
+    for cur_map in loadingTimes.keys():
+        average_loading_times.append(np.average(loadingTimes[cur_map]))
+        map_translations.append(possibleMaps[cur_map])
+    data_frame = pd.DataFrame(dict(
+        map=map_translations,
+        average_load=average_loading_times
+    ))
+    fig = px.bar(data_frame, x="map", y="average_load")
+    fig.show()
+
+
+def shutdown():
+    drawLoadingTimesBarChart()
+
+
+def isPixelBlack(pixel):
+    return pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0
+
+
 if __name__ == "__main__":
+    atexit.register(shutdown)
     # take and show a screenshot every second
     loading_since = 0
     while True:
+        time.sleep(.2)
         img = takeDbdScreenshot()
-
         pixel = img.getpixel((128, 128))
 
-        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+        if isPixelBlack(pixel):
             if loading_since == 0:
                 loading_since = time.time()
-        elif loading_since != 0:
-            totalLoad = time.time() - loading_since
-            print("Total loading time: " + str(totalLoad))
-            if time.time() - loading_since > 10:
-                print("Loading took too long")
-                foundMap = ""
-                mapId = 0
-                for curMap in possibleMaps:
-                    mapId += 1
-                    print(str(mapId) + ". " + possibleMaps[curMap])
-                while not foundMap.isdigit():
-                    mapId = input("Enter map number: ")
-                    if mapId.isdigit():
-                        mapId = int(mapId)
-                        if 0 < mapId <= len(possibleMaps):
-                            foundMap = list(possibleMaps.keys())[mapId - 1]
-                            break
+            continue
+        elif loading_since == 0:
+            continue
 
-                # append totalLoad to dict loading_times with foundMap as key
-                if foundMap in loadingTimes:
-                    loadingTimes[foundMap].append(totalLoad)
-                else:
-                    loadingTimes[foundMap] = [totalLoad]
+        totalLoad = time.time() - loading_since
+        print("Total loading time: " + str(totalLoad))
 
-                # save dict loading_times to json file
-                with open("loadingTimes.json", "w") as outfile:
-                    json.dump(loadingTimes, outfile, indent=2)
-                print("Average loading time for " + possibleMaps[foundMap] + ": " +
-                      str(np.average(loadingTimes[foundMap])))
-            else:
-                print("Loading time too short")
-
+        if totalLoad < 10:
+            print("Loading time too short")
             loading_since = 0
+            continue
 
-        time.sleep(.2)
+        # find the map
+        foundMap = ""
+        mapId = 0
+        for curMap in possibleMaps:
+            mapId += 1
+            print(str(mapId) + ". " + possibleMaps[curMap])
+        mapId = ""
+        while not mapId.isdigit() or int(mapId) < 1 or int(mapId) > len(possibleMaps):
+            mapId = input("Enter map number: ")
+        mapId = int(mapId)
+        foundMap = list(possibleMaps.keys())[mapId - 1]
+
+        if foundMap in loadingTimes:
+            loadingTimes[foundMap].append(totalLoad)
+        else:
+            loadingTimes[foundMap] = [totalLoad]
+
+        # save dict loading_times to json file
+        with open("loadingTimes.json", "w") as outfile:
+            json.dump(loadingTimes, outfile, indent=2)
+        print("Average loading time for " + possibleMaps[foundMap] + ": " +
+              str(np.average(loadingTimes[foundMap])))
+        loading_since = 0
